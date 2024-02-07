675fbea42ff5aa436ff0483b094e3089da10a8f6
RenujaSathnidu <145992554+RenujaSathnidu@users.noreply.github.com>
2024-02-07 06:37:05 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* (1)
+    float F;
+    float C;
+
+    printf("Input Celsius Temperature :");
+    scanf("%f",&C);
+
+    F=(1.8*C)+32;
+
+    printf("Franhiet Temperature : %f",F);
+
+    (2)
+    int I,Y,F;
+    printf("Input length in inches : ");
+    scanf("%d",&I);
+
+    Y=I/36;
+    F=((I%36)/12);
+    I=((I%36)%12);
+
+    printf("Length : Yards %d",Y);
+    printf("Feets %d ",F);
+    printf("Inches %d \n",I);
+
+    return 0;
+    (3)
+
+    float F,I,X,C;
+
+    printf("Input length in feet and inches :");
+    scanf("%f %f",&F,&I);
+
+    X=F*12.0;
+    I=X+F;
+
+    C=I*2.54;
+
+    printf("Length in centimeters : %f \n",C);
+
+    return(0);*/
+
+    /*(4)
+    int a,b,c,s;
+    float A;
+
+    printf("Input a,b,c :");
+    scanf("%d %d %d",&a,&b,&c);
+
+    s=(a+b+c)/2;
+
+    A=((s*(s-a)*(s-b)*(s-c))^(1/2));
+
+    printf("Area =%f ",A);
+
+    return(0); */
+
+    /*
+    (5)
+    int n1,n2;
+
+    printf("Input num1 :");
+    scanf("%d",&n1);
+
+    printf("Input num2 :");
+    scanf("%d",&n2);
+
+    int x=n1;
+    n1=n2;
+    n2=x;
+
+    printf("Num 1:%d , Num 2:%d",n1,n2);
+
+    return(0);*/
+    /*
+
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+    float d;
+
+
+    printf("Point 1:");
+    scanf("%d,%d",&x1,&y1);
+
+
+    printf("Point 2:");
+    scanf("%d,%d",&x2,&y2);
+
+    d=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+
+
+    (8)
+
+    printf("Distance =%.2f",d);
+    */
+    /*
+    int x1,y1,x2,y2;
+    float d,p;
+    printf("Enter the cordinates : ");
+    scanf("(%d,%d),(%d,%d)",&x1,&y1,&x2,&y2);
+
+    d=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+
+    p=2*3.14*(d/2);
+
+    printf("Perimeter :%.2f",p);
+
+    return(0);
+
+    (10)
+    int a,b,c;
+
+    printf("Enter the price of 12 mangoes =");
+    scanf("%d",&a);
+
+    b=(a/12)*28;
+    c=(a%12)*28;
+
+    printf("Price Rupees %d and Cents %d",b,c);
+
+    return(0); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
